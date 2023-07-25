f1278cb5f619bb6b4b4ca77670a5edc22907fbb8
superviss <82835778+superviss@users.noreply.github.com>
2023-07-25 08:09:46 +0700

    Update Humanverify.py

--- Humanverify.py
+++ Humanverify.py
@@ -7,5 +7,15 @@ else
   end
 if x < 2 then 
   x = x + 1
+x = 2
+def sigmoid_deriv(x):
+return sigmoid(x) * (1-sigmoid(x))
+
+derror_dprediction = 2 * (prediction - target)
+layer_1 = np.dot(input_vector, weights_1) + bias
+dprediction_dlayer1 = sigmoid_deriv(layer_1)
+dlayer1_dbias = 1
+
+
 
 
